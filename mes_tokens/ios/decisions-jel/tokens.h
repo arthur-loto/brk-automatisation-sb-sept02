1c0a3ff16109a36434eca92aefec228e56523462
arthur-loto <arthur.rigaud@loto-quebec.com>
2022-10-05 18:14:10 +0000

    [MAJ_Auto] Après livraison NPM

--- mes_tokens/ios/decisions-jel/tokens.h
+++ mes_tokens/ios/decisions-jel/tokens.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 05 Oct 2022 17:23:26 GMT
+// Generated on Wed, 05 Oct 2022 18:12:54 GMT
 
 
 #import <Foundation/Foundation.h>
@@ -842,4 +842,30 @@
 #define BrkPromoTertiaryXlHeadingParagraphSpacing 0
 #define BrkPromoTertiaryXlHeadingTextCase none
 #define BrkPromoTertiaryXlHeadingTextDecoration none
+#define BrkButtonIconStandardFontColorDefault #535353
+#define BrkButtonIconStandardBackgroundColorDefault #ffffff
+#define BrkButtonIconStandardBorderColorDefault #e6e6e6
+#define BrkButtonIconStandardFontColorHover #1a1a1a
+#define BrkButtonIconStandardBackgroundColorHover #ffffff
+#define BrkButtonIconStandardBorderColorHover #535353
+#define BrkButtonIconStandardFontColorFocus #1a1a1a
+#define BrkButtonIconStandardBackgroundColorFocus #ffffff
+#define BrkButtonIconStandardBorderColorFocus #535353
+#define BrkButtonIconInvertedFontColorDefault #e6e6e6
+#define BrkButtonIconInvertedBackgroundColorDefault #1a1a1a
+#define BrkButtonIconInvertedBorderColorDefault #535353
+#define BrkButtonIconInvertedFontColorHover #ffffff
+#define BrkButtonIconInvertedBackgroundColorHover #1a1a1a
+#define BrkButtonIconInvertedBorderColorHover #eaedfa
+#define BrkButtonIconInvertedFontColorFocus #ffffff
+#define BrkButtonIconInvertedBackgroundColorFocus #1a1a1a
+#define BrkButtonIconInvertedBorderColorFocus #ffffff
+#define BrkButtonIconSmSize 30
+#define BrkButtonIconSmIconHeight 8
+#define BrkButtonIconMdSize 40
+#define BrkButtonIconMdIconHeight 16
+#define BrkButtonIconLgSize 50
+#define BrkButtonIconLgIconHeight 20
+#define BrkButtonIconXlSize 60
+#define BrkButtonIconXlIconHeight 24
 
--- mes_tokens/ios/decisions-moj/tokens.h
+++ mes_tokens/ios/decisions-moj/tokens.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 05 Oct 2022 17:23:26 GMT
+// Generated on Wed, 05 Oct 2022 18:12:54 GMT
 
 
 #import <Foundation/Foundation.h>
