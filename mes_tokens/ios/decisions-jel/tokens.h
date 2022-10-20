818bc369e132d2c837d3297123218c18d3de1065
arthur-loto <arthur.rigaud@loto-quebec.com>
2022-10-20 20:43:02 +0000

    [MAJ_Auto] Après livraison NPM

--- mes_tokens/ios/decisions-jel/tokens.h
+++ mes_tokens/ios/decisions-jel/tokens.h
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Thu, 20 Oct 2022 20:28:36 GMT
+// Generated on Thu, 20 Oct 2022 20:41:46 GMT
 
 
 #import <Foundation/Foundation.h>
@@ -878,7 +878,7 @@
 #define BrkButtonIconInvertedBoxShadowHover [object Object]
 #define BrkButtonIconInvertedBoxShadowFocus [object Object]
 #define BrkPageStandardSubheadingFontColor #000000
-#define BrkPromoTetairyVerticalAlignementTop 26
-#define BrkPromoTetairyVerticalAlignementCenter 172
-#define BrkPromoTetairyVerticalAlignementBottom 172
+#define BrkPromoTetairyVerticalAlignementTop flex_start
+#define BrkPromoTetairyVerticalAlignementCenter center
+#define BrkPromoTetairyVerticalAlignementBottom flex_end
 
